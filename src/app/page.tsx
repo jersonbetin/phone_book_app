e6febdeba3f48d9e8cf6bd294e85e4dc12c02dfa
jbetinpantoja <jbetin@blossom.net>
2023-04-04 15:55:03 -0500

    feat: find by phone, last and first name

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,15 @@
 "use client";
 
 import { Inter } from "next/font/google";
-import { Col, Container, Row } from "react-bootstrap";
+import { Col, Container, Form, Row } from "react-bootstrap";
 
 import PhoneForm from "./components/PhoneForm";
 import { useLocalStorage } from "./hooks/useLocalStorage";
 import { IPhone } from "./interfaces/phone";
 import CustomTable from "./components/CustomTable";
-import { ColumnDefinitionType } from "./types/commons";
 import { useState } from "react";
 import ToastDefault from "./components/ToastDefault";
+import PhoneList from "./components/PhoneList";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -26,22 +26,6 @@ export default function Home() {
     setList([...list, item]);
     setToast({ show: true, message: "Info was added successfully!" });
   };
-
-  const columns: ColumnDefinitionType<IPhone, keyof IPhone>[] = [
-    {
-      key: "firstName",
-      header: "First Name",
-    },
-    {
-      key: "lastName",
-      header: "Last Name",
-    },
-    {
-      key: "phone",
-      header: "Phone",
-    },
-  ];
-
   const onHandleEdit = (item: IPhone) => {
     setCurrent(item);
   };
@@ -66,22 +50,23 @@ export default function Home() {
     <main>
       <Container fluid>
         <Row className="mt-5">
-          <Col lg={4} className="px-xl-5">
+          <Col md={4} lg={3} className="px-xl-5">
             <PhoneForm
               onSubmit={onSubmit}
               current={current}
               onUpdate={onHandleUpdate}
             />
           </Col>
-          <Col lg={8} className="border-danger">
-            <div>
-              <CustomTable
-                data={list}
-                columns={columns}
-                onDelete={onHandleDelete}
-                onEdit={onHandleEdit}
-              />
-            </div>
+          <Col
+            md={8}
+            lg={9}
+            className="style-list  divider-left padding-list-md"
+          >
+            <PhoneList
+              data={list}
+              onDelete={onHandleDelete}
+              onEdit={onHandleEdit}
+            />
           </Col>
         </Row>
       </Container>
